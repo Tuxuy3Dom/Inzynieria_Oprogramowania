afdd3e55890a3a3e2276bb99c4861375df9327c1
Ihor Yaremchuk <w61863@student.wsiz.edu.pl>
2023-04-17 14:23:50 +0200

    New update

---
+++ Lesson 5/Wzorzecprojektowy/IProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wzorzecprojektowy
+{
+    // The Product interface declares the operations that all concrete products
+    // must implement.
+    public interface IProduct
+    {
+        string Operation();
+    }
+
+    // Concrete Products provide various implementations of the Product
+    // interface.
+}
