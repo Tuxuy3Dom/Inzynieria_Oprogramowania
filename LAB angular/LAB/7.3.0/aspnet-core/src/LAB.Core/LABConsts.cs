92b170455ebc4e45a7953f7f0fe11684c1cfeffb
Ihor Yaremchuk <w61863@student.wsiz.edu.pl>
2023-05-08 14:10:26 +0200

    Project in ASPNET Boiller

---
+++ LAB angular/LAB/7.3.0/aspnet-core/src/LAB.Core/LABConsts.cs
@@ -0,0 +1,20 @@
+﻿using LAB.Debugging;
+
+namespace LAB
+{
+    public class LABConsts
+    {
+        public const string LocalizationSourceName = "LAB";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "a1a2051bfaef45c89f242eb75f48eeeb";
+    }
+}
